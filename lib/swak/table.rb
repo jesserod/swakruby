d7360d9eed33497dd51d4bbcf9ad1d356cf1eb77
Jesse Rodriguez <jesserod@cs.stanford.edu>
2012-07-22 13:40:21 -0400

    added Table::SmartRow, updated and refactored table tests

--- lib/swak/table.rb
+++ lib/swak/table.rb
@@ -1,43 +1,84 @@
 module Swak
   module Table
-    def Table.read(io_or_fn, opts={:has_header => false, :type => String, :delim => "\t"})
+    class SmartRow
+      attr_reader :header, :fields, :colmap
+
+      def initialize(header, fields)
+        if header.size != fields.size
+          raise "Header and field size mismatch in SmartRow.new #{header.inspect} vs #{fields.inspect}"
+        end
+
+        @header = header
+        @fields = fields
+        @colmap = {}
+        header.each_with_index {|fieldname, i| @colmap[fieldname] = i}
+        if @colmap.size != @header.size
+          raise "Duplicate field names not allowed in SmartRow header"
+        end
+      end
+
+      def [](*args)
+        if args.size == 1 && args[0].is_a?(String)
+          @fields[@colmap[args[0]]]
+        else
+          @fields[*args]
+        end
+      end
+
+      def []=(*args)
+        if args.size == 2 && args[0].is_a?(String)
+          @fields[@colmap[args[0]]] = args[-1]
+        else
+          @fields.[]=(*args)
+        end
+      end
+
+      def method_missing(sym, *args, &block)
+        @fields.send(sym, *args, &block)
+      end
+    end
+
+
+    def Table.read(io_or_fn, opts={:has_header => false, :type => String, :delim => "\t", :smart => false})
       if io_or_fn.is_a?(String)
         io = File.new(io_or_fn)
       else
         io = io_or_fn
       end
-      if opts[:has_header]
-        header = io.gets 
-        opts[:header] = header.chomp
+      if opts[:has_header] || opts[:smart]
+        header = io.gets.chomp 
+        opts[:header] = header
       end
       
       table = []
       delim = opts[:delim] || "\t"
 
-      if (opts[:type] == Integer)
-        for line in io
-          line.chomp!
-          f = line.split(delim)
-          table << f.map{|x| x.to_i_strict}
-        end
-      elsif (opts[:type] == Float)
-        for line in io
-          line.chomp!
-          f = line.chomp.split(delim)
-          table << f.map{|x| x.to_f_strict}
-        end
-      else # Assume String
-        for line in io
-          line.chomp!
-          table << line.split(delim)
-        end
+      case opts[:type].to_s
+      when "Integer"
+        ele_proc = Proc.new{|ele| ele.to_i_strict}
+      when "Float"
+        ele_proc = Proc.new{|ele| ele.to_f_strict}
+      else
+        ele_proc = Proc.new{|ele| ele} # By default, don't format
+      end
+
+      if opts[:smart]
+        row_proc = Proc.new{|row| SmartRow.new(header.split(delim), row) }
+      else
+        row_proc = Proc.new{|row| row}
+      end
+
+      for line in io
+        line.chomp!
+        f = line.split(delim)
+        table << row_proc.call(f.map(&ele_proc))
       end
 
       return table
     end
 
     def Table.write(table, io_or_fn, opts={:header => nil, :fmt => nil, :delim => "\t"})
-      raise "Illegal fmt in Swak::Table.write" if !opts[:fmt].nil? && !opts[:fmt].is_a?(String)
+      raise "Illegal fmt in Swak::Table.write: #{opts.inspect}" if !opts[:fmt].nil? && !opts[:fmt].is_a?(String)
 
       delim = opts[:delim] || "\t"
 
@@ -68,6 +109,7 @@ module Swak
 
     def Table.transpose(table)
       raise "Error in Swak::Table.transpose(): Cannot transpose nil table" if table.nil?
+      raise "Error in Swak::Table.transpose(): Cannot transpose a SmartRow table (yet)" if table.size > 0 && table[0].is_a?(SmartRow)
       return [] if table.size == 0
 
       num_rows = table.size
@@ -91,6 +133,10 @@ module Swak
       end
 
       return out_mat
+    end
+
+    def Table.column(table, col)
+      table.map {|row| row[col]}
     end
   end
 end
--- test/test_table.rb
+++ test/test_table.rb
@@ -18,25 +18,40 @@ puts
 puts "############# Testing reading ##############"
 
 # Happy cases, as strings
-pp Swak::Table.read(fn("ints_header.tsv"))
-pp Swak::Table.read(fn("ints_header.tsv"), :delim => /\s+/)
+puts "== Testing Strings ==".color('m')
+pp(table = Swak::Table.read(fn("ints_header.tsv")))
+raise unless Swak::Table.read(fn("ints_header.tsv"), :delim => /\s+/) == table
 hh = {:has_header => true}
-pp Swak::Table.read(fn("ints_header.tsv"), hh)
-pp hh
+raise if Swak::Table.read(fn("ints_header.tsv"), hh) == table
+raise unless hh[:header].is_a?(String)
+puts "Output opts from header: #{hh.inspect}"
 
 # Happy case integer
-pp Swak::Table.read(fn("ints.tsv"), :type => Integer)
-pp Swak::Table.read(fn("ints_header.tsv"), :type => Integer, :has_header => true)
+puts "== Testing Integer ==".color('m')
+pp(table=Swak::Table.read(fn("ints.tsv"), :type => Integer))
+raise unless table[0][0].is_a?(Integer)
+raise unless Swak::Table.read(fn("ints_header.tsv"), :type => Integer, :has_header => true) == table
 
 # Happy case float
-pp Swak::Table.read(fn("floats.tsv"), :type => Float)
-pp Swak::Table.read(fn("floats_header.tsv"), :type => Float, :has_header => true)
+puts "== Testing SmartRow ==".color('m')
+pp(table = Swak::Table.read(fn("floats.tsv"), :type => Float))
+raise unless table[0][0].is_a?(Float)
+raise unless Swak::Table.read(fn("floats_header.tsv"), :type => Float, :has_header => true) == table
+
+# Happy case smart float
+puts "== Testing SmartRow ==".color('m')
+table = Swak::Table.read(fn("floats_header.tsv"), :smart => true)
+raise unless table[0].is_a?(Swak::Table::SmartRow)
+raise unless Swak::Table.read(fn("floats_header.tsv"), :smart => true)[0]["col1"].is_a?(String)
+puts "OK"
+
 
 begin 
-  print "Testing for error...  "
+  puts "== Testing for error ==".color('m')
   pp Swak::Table.read(fn("floats_header.tsv"), :type => Float)
+  puts "FAIL".color('r', false)
 rescue Exception => e
-  puts "OK! (#{e.inspect})"
+  puts "OK! (#{e.inspect})".color('g')
 end
 
 puts "############# Testing writing ##############"
@@ -46,31 +61,32 @@ table =  Swak::Table.read(fn("floats_header.tsv"), hh)
 file = fn("out.tsv")
 
 # Happy cases
-puts
-Swak::Table.write(table, file, hh[:header])
+puts "== Testing as tab-delim, strings, no header) ==".color("m") 
+Swak::Table.write(table, file)
 puts File.read(file)
 
-puts
+puts "== Testing as with header ==".color("m") 
 Swak::Table.write(table, file, {:header => hh[:header].split})
 puts File.read(file)
 
-puts
+puts "== Testing as header, commas ==".color("m") 
 Swak::Table.write(table, file, {:header => hh[:header].split, :delim => ","})
 puts File.read(file)
 
-puts
+puts "== Testing as format %f ==".color("m") 
 Swak::Table.write(table, file, {:header => hh[:header], :fmt => "%f"})
 puts File.read(file)
 
-puts
+puts "== Testing as format %.1f ==".color("m") 
 Swak::Table.write(table, file, {:header => hh[:header], :fmt => "%.1f"})
 puts File.read(file)
 
 begin 
-  print "Testing for error..."
+  puts "== Testing for error ==".color("m") 
   Swak::Table.write(table, file, {:fmt => Integer})
+  puts "FAIL".color('r')
 rescue Exception => e
-  puts "OK! (#{e.inspect})"
+  puts "OK! (#{e.inspect})".color('g')
 end
 
 File.delete(file)
